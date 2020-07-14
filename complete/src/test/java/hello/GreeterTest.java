b5b3961e7368065a0ba35433af5a7cd6ee570cb4
sidvijay18 <vijaysiddharth89@gmail.com>
2020-07-14 09:13:43 +0530

    Update GreeterTest.java

--- complete/src/test/java/hello/GreeterTest.java
+++ complete/src/test/java/hello/GreeterTest.java
@@ -11,7 +11,7 @@ public class GreeterTest {
 
 	@Test
 	public void greeterSaysHello() {
-		assertThat(greeter.sayHello(), containsString("Yellow"));
+		assertThat(greeter.sayHello(), containsString("Hello"));
 	}
 
 }
